2d691398c2e4857f640221aff3dbd2737082a53d
Anas-hisham <158775366+Anas-hisham@users.noreply.github.com>
2025-02-01 13:17:51 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from "react"; // Add useEffect
+import { useState, useEffect } from "react";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/Footer";
@@ -32,15 +32,31 @@ export default function RootLayout({ children }) {
   const removeFromCart = (index) => {
     setCart((prevCart) => prevCart.filter((_, i) => i !== index));
   };
+
   return (
-      <CartContext.Provider value={{ cart, setCart, removeFromCart }}>
-        <html lang="en">
-          <body className={`${roboto.variable} font-sans antialiased`}>
-            <Header />
+    <CartContext.Provider value={{ cart, setCart, removeFromCart }}>
+      <html lang="en">
+      <head>
+          <title>Mastering Frontend</title>
+          <meta 
+            name="description" 
+            content="Shop high-quality front-end development courses. Learn HTML, CSS, Bootstrap, Tailwind, JavaScript, ES6, React, Vite, and Next.js to become a skilled web developer." 
+          />
+          <meta name="keywords" content="HTML, CSS, Bootstrap, Tailwind, JavaScript, ES6, React, Vite, Next.js, Front-End Development, Web Development Courses" />
+          <meta name="author" content="Anas Hisham" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
+        <body className={`${roboto.variable} font-sans antialiased`}>
+          <Header>
+            <h1>Learn Front-End Programming & Shop Coding Courses</h1>
+            <h2>Master the skills you need to build modern web applications</h2>
+          </Header>
+          <main>
             {children}
-            <Footer />
-          </body>
-        </html>
-      </CartContext.Provider>
+          </main>
+          <Footer />
+        </body>
+      </html>
+    </CartContext.Provider>
   );
 }
